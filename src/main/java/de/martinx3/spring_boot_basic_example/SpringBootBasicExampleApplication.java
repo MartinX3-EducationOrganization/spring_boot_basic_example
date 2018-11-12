f75259ba05f01e0c27c09310b62988bf6c0de6c2
Martin Duenkelmann <nc-duenkekl3@netcologne.de>
2018-11-12 23:36:23 +0100

    Initial Commit
    Signed-off-by: Martin Duenkelmann <nc-duenkekl3@netcologne.de>

---
+++ src/main/java/de/martinx3/spring_boot_basic_example/SpringBootBasicExampleApplication.java
@@ -0,0 +1,12 @@
+package de.martinx3.spring_boot_basic_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBasicExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/de/martinx3/spring_boot_basic_example/SpringBootBasicExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package de.martinx3.spring_boot_basic_example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootBasicExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
